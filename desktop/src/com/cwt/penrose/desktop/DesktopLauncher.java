899ef42e005cbf966c4ca56399784327a06b9640
Max Dumas <maltor124@gmail.com>
2014-05-29 13:59:35 -0400

    Initial commit

---
+++ desktop/src/com/cwt/penrose/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cwt.penrose.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cwt.penrose.PenroseGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PenroseGame(), config);
+	}
+}
